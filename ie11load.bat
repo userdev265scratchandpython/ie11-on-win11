da7a6e35a922c686b990c7df97cf5daa805ad8c8
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-09-03 18:28:27 +0200

    Update ie11load.bat

--- ie11load.bat
+++ ie11load.bat
@@ -20,18 +20,16 @@ echo objIE.Resizable = True >> "%fpath%\random_window.vbs"
 echo objIE.Navigate "https://www.google.com/" >> "%fpath%\random_window.vbs"
 echo objShell.AppActivate objIE >> "%fpath%\random_window.vbs"
 
-echo finished writing file.
-
-timeout /t 1 > nul
-
-cls
-
-echo ie11 reloader for windows(r) 10 and 11
-
 echo opening IE11...
 
 :: Run the VBScript with the random color
-start cscript //nologo "%fpath%\random_window.vbs" %rand_color%
+start cmd /c start cscript //nologo "%fpath%\random_window.vbs" %rand_color%
+
+cls
+
+cls
+
+cls
 
 cls
 
