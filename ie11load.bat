0078f1aa1f195d0ddcc517450577b499856dba27
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-09-03 10:47:30 +0200

    updated to be better

--- ie11load.bat
+++ ie11load.bat
@@ -1,12 +1,38 @@
+@echo off
+set "fpath=C:\windows\temp"
+set /p fpath="Enter the path to put the temporary file (default is %fpath%, do not put an '\' at the end): "
+
+title internet explorer
+
+echo file writing started
+
 :: Write the VBScript code to a file
-echo Set objIE = CreateObject("InternetExplorer.Application") > random_window.vbs
-echo objIE.Visible = True >> random_window.vbs
-echo objIE.ToolBar = True >> random_window.vbs
-echo objIE.StatusBar = True >> random_window.vbs
-echo objIE.MenuBar = True >> random_window.vbs
-echo objIE.Resizable = True >> random_window.vbs
-echo objIE.Navigate "https://www.google.com/" >> random_window.vbs
-echo objShell.AppActivate objIE >> random_window.vbs
+echo Set objIE = CreateObject("InternetExplorer.Application") > "%fpath%\random_window.vbs"
+echo objIE.Visible = True >> "%fpath%\random_window.vbs"
+echo objIE.ToolBar = True >> "%fpath%\random_window.vbs"
+echo objIE.StatusBar = True >> "%fpath%\random_window.vbs"
+echo objIE.MenuBar = True >> "%fpath%\random_window.vbs"
+echo objIE.Resizable = True >> "%fpath%\random_window.vbs"
+echo objIE.Navigate "https://www.google.com/" >> "%fpath%\random_window.vbs"
+echo objShell.AppActivate objIE >> "%fpath%\random_window.vbs"
+
+echo finished writing file.
+
+timeout /t 1 > nul
+
+cls
+
+echo opening IE11...
 
 :: Run the VBScript with the random color
-cscript //nologo random_window.vbs %rand_color%
+cscript //nologo "%fpath%\random_window.vbs" %rand_color%
+
+echo opened IE11, cleaning up...
+
+del "%fpath%\random_window.vbs"
+
+cls
+
+echo press any key to quit the terminal
+
+pause > nul
