711b34a5778bb30bc8dbaf710a167e60780904fa
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-09-21 10:44:17 +0200

    updated

--- ie11/credits-print.bat
+++ ie11/credits-print.bat
@@ -1,10 +1,13 @@
-@echo
 :: creator : userdev265scratchandpython
 :: commit date : 04/09/24
 :: vertion of the credits script : v1.2
 :: made in batch
 :: licence : GPL 3.0
 :: Use With Caution
+
+@echo off
+color 0f
+cls
 
 echo owner of repository : userdev265scratchandpython
 echo creator of Internet Explorer : Microsoft Corporation
